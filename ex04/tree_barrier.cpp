67f2533ac2563a8131cdf1293c58d7cd44df26d1
Justin Sostmann <justin.sostmann@googlemail.com>
2023-12-03 15:19:53 +0100

    Fix tree barrier

--- ex04/tree_barrier.cpp
+++ ex04/tree_barrier.cpp
@@ -7,39 +7,48 @@
 
 #include "time_experiment.hh"
 
-const int P = 4;                              // number of threads and number of leafs
-std::vector<std::atomic_int> sync_levels(P);  // counter shared between 2 threads for one index
-std::atomic_int counter = 0;                  // shared counter
+const int P = 4;  // number of threads and number of leafs
+std::vector<std::atomic_int> sync_counter(P);
+std::vector<bool> sleep_flag(P, false);
+std::atomic_int counter = 0;  // shared counter
 
 // Combining Tree Barrier
 void f(int rank) {
-    if (P < 3) {
+    if (P == 1) {
         counter++;
         return;
     }
 
-    int level = 0;
-    int index = rank;
-    int partner = 0;
-    int sync_index = 0;  // sync to this index
+    sync_counter[rank] = 1;  // initialize own value
 
-    if (index % 2 == 0) {
-        partner = index + 1;
-        sync_index = index / 2;
-    } else {
-        partner = index - 1;
-        sync_index = (index - 1) / 2;
+    int partner{};
+    for (int k = 0; k < std::log2(P); k++) {
+        partner = std::pow(2, k) + rank;
+
+        if ((rank & (1 << k)) >> k) {
+            sleep_flag[rank] = true;
+            break;
+        }
+
+        if (partner <= P) {
+            while (!sleep_flag[partner]) {}  // wait for sleep of partner
+            sync_counter[rank] += sync_counter[partner];
+        }
     }
 
-    sync_levels[index]++;  // each thread initializes its own index
-    // wait for partner to initialize its index
-    while (sync_levels[partner] == 0) {
+    if (!sleep_flag[rank]) {
+        counter += sync_counter[rank];
     }
 
-    
+    while (sleep_flag[rank]) {}
 
+    for (int k = std::log2(P) - 1; k >= 0; k--) {
+        partner = std::pow(2, k) + rank;
 
-
+        if (partner <= P) {
+            sleep_flag[partner] = false;
+        }
+    }
 }
 
 void fill_atomic_vector(std::vector<std::atomic_int>& vec, int value) {
@@ -50,7 +59,7 @@ void fill_atomic_vector(std::vector<std::atomic_int>& vec, int value) {
 
 int main() {
     std::vector<std::thread> threads;
-    fill_atomic_vector(sync_levels, 0);
+    fill_atomic_vector(sync_counter, 0);
 
     for (int rank = 0; rank < P; ++rank) {
         threads.push_back(std::thread{f, rank});
